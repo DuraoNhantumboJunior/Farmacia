34aac0d463ae3c816b9dd40c9e8ae592157e52e0
DuraoNhantumboJunior <duraojnr@gmail.com>
2024-10-20 00:26:02 +0200

    ":refactor: Organização da base de dados"

---
+++ database/migrations/2024_10_17_094101_create_table_stock_produto.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_produtos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('fornecedor_id')->constrained('fornecedores')->onDelete('cascade');;
+            $table->integer('produto_id')->constrained('produtos')->onDelete('cascade');;
+            $table->integer('quantidade');
+            $table->decimal('preco,8,2');
+            $table->date('validade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_produtos');
+    }
+};
