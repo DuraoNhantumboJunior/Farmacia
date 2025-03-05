0182fb3048765949f38d69c97f595f5bb31032c5
DuraoNhantumboJunior <duraojnr@gmail.com>
2025-03-05 10:42:08 +0200

    feat:"Alterar permissoes e estado backend"

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\Venda;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
@@ -28,12 +29,13 @@ class UsersController extends Controller
         // Obtém o ano corrente e o mês atual
         $anoCorrente = Carbon::now()->year;
         $mesCorrente = Carbon::now()->month;
-    
+
         $users = User::all()->count();
         $stock = Stock::all()->count();
         $fornecedores = Fornecedore::all()->count();
         $medicamentos = Medicamento::all()->count();
-        
+        $auth = Auth::user()->type;
+
         // Consulta as vendas do ano corrente, agrupando por mês
         $vendasPorMes = DB::table('vendas')
             ->select(DB::raw('MONTH(created_at) as mes'), DB::raw('SUM(total) as total'))
@@ -41,31 +43,59 @@ class UsersController extends Controller
             ->groupBy(DB::raw('MONTH(created_at)'))
             ->orderBy('mes', 'asc')
             ->get();
-    
+
         // Inicialize o acumulado de vendas
         $acumulado = 0;
         $vendasAcumuladas = [];
-    
+
         // Preenche os meses anteriores ao mês atual com 0
         for ($i = 1; $i < $mesCorrente; $i++) {
             $vendasAcumuladas[] = 0;  // Preenche com 0 para os meses anteriores
         }
-    
+
         // Adiciona as vendas para os meses registrados
         foreach ($vendasPorMes as $venda) {
             $acumulado += $venda->total;
             $vendasAcumuladas[] = $acumulado;
         }
-    
+
         // Preenche os meses futuros com 0
         for ($i = count($vendasAcumuladas); $i < 12; $i++) {
             $vendasAcumuladas[] = 0;  // Preenche com zero nos meses futuros
         }
-        
-        Log::info('vendas', $vendasAcumuladas);
-    
-        return view('dashboard', compact('users', 'medicamentos', 'fornecedores', 'stock', 'vendasAcumuladas'));
-    }
-    
 
+        return view('dashboard', compact('users', 'medicamentos', 'fornecedores', 'stock', 'vendasAcumuladas','auth'));
+    }
+
+    public function alterarEstado($id)
+    {
+        $user = User::findOrFail($id);
+
+        // Verifica se o usuário é do tipo 'Master'
+        if ($user->type === 'Master') {
+            return response()->json(['message' => 'Não é possível alterar o estado do gerente (Master).'], 400);
+        }
+
+        // Alterna o estado se não for 'Master'
+        $user->estado = $user->estado === 'activo' ? 'desativado' : 'activo';
+        $user->save();
+
+        return response()->json(['message' => 'Estado do usuário atualizado com sucesso!']);
+    }
+
+    public function alterarNivel($id)
+    {
+        $user = User::findOrFail($id);
+
+        // Verifica se o usuário é do tipo 'Master'
+        if ($user->type === 'Master') {
+            return response()->json(['message' => 'Não é possível alterar as permissões do gerente (Master).'], 400);
+        }
+
+        // Alterna o tipo de acesso se não for 'Master'
+        $user->type = $user->type === 'normal' ? 'admin' : 'normal';
+        $user->save();
+
+        return response()->json(['message' => 'Nível de acesso atualizado com sucesso!']);
+    }
 }
